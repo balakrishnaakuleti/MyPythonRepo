fc66ba9df20a451e357e69bc7c15e906c8ec5e54
Balakrishna Akuleti <Balakrishna Akuleti@INLN50930358A.apj.global.corp.sap>
2018-06-05 15:20:32 +0530

    code push

--- src/ChartStyling.py
+++ src/ChartStyling.py
@@ -11,7 +11,32 @@ import pylab
 
 x = np.arange(0,10)
 print(x)
-y = x*x
+y = x^2
+z = x^3
+t = x^4
 print(y)
-plt.plot(x,y)
+plt.plot(x,y,'g')
+#plt.plot(x,y,'>')
+# Titles and Axis Labels
+plt.title('Time Vs Speed Graph')
+plt.xlabel('Time')
+plt.ylabel('Speed')
+
+#Annotating points
+plt.annotate(xy=[0,2], s='First Entry')
+plt.annotate(xy=[4,6], s='Second Entry')
+
+#Plotting multiple graphs
+plt.plot(x,z)
+plt.plot(x,t)
+
+#Legend
+plt.legend(['x','y','z'], loc=4)
+
+#Styling 
+
+plt.style.use('fast')
+plt.plot(x,z)
+
+
 pylab.show()
